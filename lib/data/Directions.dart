8cf1505fdf23c30909c967dc833dc1516d9ad2d0
Felipe Neves <felipe.brito@autonomoz.com.br>
2022-05-23 10:48:00 -0300

    google maps api

---
+++ lib/data/Directions.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:pet_tracker_ifmt/data/Directions_model.dart';
+
+class DirectionsRepository {
+  static const String _baseUrl =
+      'https://maps.googleapis.com/maps/api/directions/json?';
+
+  final Dio _dio;
+
+  DirectionsRepository({Dio dio}) : _dio = dio ?? Dio();
+
+  Future<Directions> getDirections({
+    @required LatLng origin,
+    @required LatLng destination,
+  }) async {
+    final response = await _dio.get(
+      _baseUrl,
+      queryParameters: {
+        'origin': '${origin.latitude},${origin.longitude}',
+        'destination': '${destination.latitude},${destination.longitude}',
+        'key': 'AIzaSyBM_9Go7X4TML51pFp0DOk7itbzFlY7Y7k',
+      },
+    );
+
+    // Check if response is successful
+    if (response.statusCode == 200) {
+      return Directions.fromMap(response.data);
+    }
+    return null;
+  }
+}
